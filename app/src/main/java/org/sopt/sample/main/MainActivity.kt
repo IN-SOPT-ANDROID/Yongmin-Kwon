b8af6d88dfb34dffe854ea8b8cc9d9485311730a
권용민 <briandr@naver.com>
2022-10-03 05:43:26 +0900

    make myPage Logic! 필수과제 끝!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!

--- app/src/main/java/org/sopt/sample/main/MainActivity.kt
+++ app/src/main/java/org/sopt/sample/main/MainActivity.kt
@@ -3,10 +3,24 @@ package org.sopt.sample.main
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import org.sopt.sample.R
+import org.sopt.sample.databinding.ActivityLoginBinding
+import org.sopt.sample.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding : ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        initText()
+    }
+
+    private fun initText(){
+        val id = intent.getStringExtra("id")
+        val mbti = intent.getStringExtra("mbti")
+        binding.textMypageName.text = getString(R.string.myPageName, id)
+        binding.textMypageMbti.text = getString(R.string.myPageMbti, mbti)
     }
 }
