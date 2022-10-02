e2ca712f60800595ac1c1a9a13bc532230b6cbb8
권용민 <briandr@naver.com>
2022-10-03 04:51:07 +0900

    어라... 이상하다.. 나 이거 아까 커밋한 것 같은데 왜 이렇게 돼있지??

---
+++ app/src/main/java/org/sopt/sample/auth/LoginActivity.kt
@@ -0,0 +1,82 @@
+package org.sopt.sample.auth
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.activity.result.ActivityResult
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AppCompatActivity
+import org.sopt.sample.R
+import org.sopt.sample.databinding.ActivityLoginBinding
+import org.sopt.sample.defaultSnackbar
+import org.sopt.sample.main.MainActivity
+import org.sopt.sample.shortToast
+
+class LoginActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityLoginBinding
+    private lateinit var signUpLauncher: ActivityResultLauncher<Intent>
+    private var id: String? = null
+    private var pw: String? = null
+    private var mbti: String? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        launchSignUpResult()
+        clickSignUp()
+        clickLogin()
+    }
+
+    private fun clickSignUp() {
+        binding.buttonLoginSignUp.setOnClickListener {
+            val signUpIntent = Intent(this, SignUpActivity::class.java)
+            signUpLauncher.launch(signUpIntent)
+        }
+    }
+
+    private fun clickLogin() {
+        binding.buttonLoginLogin.setOnClickListener {
+            if (!isLoginValid()) return@setOnClickListener
+            val mainIntent = Intent(this, MainActivity::class.java)
+            startActivity(mainIntent)
+        }
+    }
+
+    private fun isLoginValid(): Boolean {
+        if (id == null) {
+            shortToast(R.string.needSignUp)
+            return false
+        } else if ((binding.editLoginId.text.toString() != id) and (binding.editLoginPw.text.toString() != pw)) {
+            shortToast(R.string.checkIdPw)
+            return false
+        } else if (binding.editLoginId.text.toString() != id) {
+            shortToast(R.string.checkId)
+            return false
+        } else if (binding.editLoginPw.text.toString() != pw) {
+            shortToast(R.string.checkPw)
+            return false
+        } else {
+            shortToast(R.string.succeedSignIn)
+            return true
+        }
+    }
+
+    private fun launchSignUpResult() {
+        signUpLauncher =
+            registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {
+                getResultFromSignUp(it)
+            }
+    }
+
+    private fun getResultFromSignUp(result: ActivityResult) {
+        if (result.resultCode == RESULT_OK) {
+            id = result.data?.getStringExtra("id")
+            pw = result.data?.getStringExtra("pw")
+            mbti = result.data?.getStringExtra("mbti")
+            binding.root.defaultSnackbar(R.string.succeedSignUp)
+        } else binding.root.defaultSnackbar(R.string.failSignUp)
+    }
+}
---
+++ app/src/main/java/org/sopt/sample/auth/SignUpActivity.kt
@@ -0,0 +1,72 @@
+package org.sopt.sample.auth
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import org.sopt.sample.R
+import org.sopt.sample.databinding.ActivitySignUpBinding
+import org.sopt.sample.shortSnackbar
+
+class SignUpActivity : AppCompatActivity() {
+    private lateinit var binding: ActivitySignUpBinding
+    private val mbtiList: List<String> = listOf(
+        "ESTJ", "ESTP", "ESFJ", "ESFP", "ENTJ", "ENTP", "ENFJ", "ENFP",
+        "ISTJ", "ISTP", "ISFJ", "ISFP", "INTJ", "INTP", "INFJ", "INFP"
+    )
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivitySignUpBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        clickSignUp()
+    }
+
+    private fun clickSignUp() {
+        binding.buttonSignUpSignUp.setOnClickListener{
+            if(!isSignUpValid()) return@setOnClickListener
+            val intent = Intent(this, LoginActivity::class.java)
+            intent.putExtra("id", binding.editSignUpId.text.toString())
+            intent.putExtra("pw", binding.editSignUpPw.text.toString())
+            intent.putExtra("mbti", binding.editSignUpMbti.text.toString())
+            setResult(RESULT_OK, intent)
+            finish()
+        }
+    }
+
+    private fun isSignUpValid() : Boolean{
+        if(!isIdValid()) return false
+        if(!isPwValid()) return false
+        if(!isMbtiValid()) return false
+        else return true
+    }
+
+    private fun isIdValid(): Boolean {
+        val idLength = binding.editSignUpId.length()
+        if ((idLength >= 6) and (idLength <= 10)) return true
+        else {
+            binding.buttonSignUpSignUp.shortSnackbar(R.string.checkId)
+            return false
+        }
+    }
+
+    private fun isPwValid(): Boolean {
+        val pwLength = binding.editSignUpPw.length()
+        if ((pwLength >= 8) and (pwLength <= 12)) return true
+        else {
+            binding.buttonSignUpSignUp.shortSnackbar(R.string.checkPw)
+            return false
+        }
+    }
+
+    private fun isMbtiValid(): Boolean {
+        val mbti = binding.editSignUpMbti.text.toString()
+        if (mbti in mbtiList) return true
+        else if (mbti.length < 4) {
+            binding.buttonSignUpSignUp.shortSnackbar(R.string.mbtiIsShort)
+            return false
+        } else {
+            binding.buttonSignUpSignUp.shortSnackbar(R.string.mbtiIsStrange)
+            return false
+        }
+    }
+}
