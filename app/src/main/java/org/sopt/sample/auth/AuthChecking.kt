609e58ae9fe5c79ab7141c9215b93f9d04a6a103
권용민 <briandr@naver.com>
2022-10-03 18:01:06 +0900

    separate authChecking code from activities
    LoginActivity처럼 toast를 보여주는 코드까지 AuthChecking 클래스로 빼는게 좋을까?
    
    그래도 toast와 snackbar는 뷰를 그려주는 영역이니까 SignUpActivity처럼 체킹만 AuthChecking 클래스에서 하고 snackbar 코드는 activity에서 처리해주는게 맞을까?
    
    뭔가 후자가 맞는 것 같긴 한디 ㅋㅋ 다들 어캐 생각하시는지?

---
+++ app/src/main/java/org/sopt/sample/auth/AuthChecking.kt
@@ -0,0 +1,49 @@
+package org.sopt.sample.auth
+
+import android.content.Context
+import org.sopt.sample.R
+import org.sopt.sample.shortToast
+
+class AuthChecking {
+    fun isSignUpIdValid(id: String): Boolean = (id.length in 6..10)
+
+    fun isSignUpPwValid(pw: String): Boolean = (pw.length in 8..12)
+
+    fun isSignUpMbtiValid(mbti: String): Int {
+        if (mbti.length < 4) return SHORT
+        else if (mbti in mbtiList) return CORRECT
+        else return STRANGE
+    }
+
+    fun isSignInValid(
+        context: Context, id: String?, pw: String?, inputId: String, inputPw: String
+    ): Boolean {
+        if (id == null) {
+            context.shortToast(R.string.needSignUp)
+            return false
+        } else if ((inputId != id) and (inputPw != pw)) {
+            context.shortToast(R.string.checkIdPw)
+            return false
+        } else if (inputId != id) {
+            context.shortToast(R.string.checkId)
+            return false
+        } else if (inputPw != pw) {
+            context.shortToast(R.string.checkPw)
+            return false
+        } else {
+            context.shortToast(R.string.succeedSignIn)
+            return true
+        }
+    }
+
+    private val mbtiList: List<String> = listOf(
+        "ESTJ", "ESTP", "ESFJ", "ESFP", "ENTJ", "ENTP", "ENFJ", "ENFP",
+        "ISTJ", "ISTP", "ISFJ", "ISFP", "INTJ", "INTP", "INFJ", "INFP"
+    )
+
+    companion object {
+        const val SHORT = 0
+        const val STRANGE = 1
+        const val CORRECT = 2
+    }
+}
