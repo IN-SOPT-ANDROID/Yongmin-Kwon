3e0fad12df8fce294c7543b2dbe053249fe1a213
권용민 <briandr@naver.com>
2022-10-05 04:42:22 +0900

    make splash activity

---
+++ app/src/main/java/org/sopt/sample/ui/SplashActivity.kt
@@ -0,0 +1,62 @@
+package org.sopt.sample.ui
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.view.animation.Animation
+import android.view.animation.AnimationUtils
+import androidx.appcompat.app.AppCompatActivity
+import org.sopt.sample.R
+import org.sopt.sample.data.MySharedPreferences
+import org.sopt.sample.databinding.ActivitySplashBinding
+import org.sopt.sample.ui.auth.SignInActivity
+import org.sopt.sample.ui.main.MainActivity
+
+class SplashActivity : AppCompatActivity() {
+    private lateinit var binding: ActivitySplashBinding
+    private lateinit var sharedPref: MySharedPreferences
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivitySplashBinding.inflate(layoutInflater)
+        sharedPref = MySharedPreferences()
+        sharedPref.init(this)
+        setName()
+        setContentView(binding.root)
+
+        setDelay()
+    }
+
+    private fun setName() {
+        val name = sharedPref.loginName ?: return
+        binding.textName.text = name
+    }
+
+    private fun checkAutoLogin() {
+        val id = sharedPref.loginId
+        val autoLogin = sharedPref.autoLogin
+        if (id == null) startLogin()
+        if (autoLogin) startMain()
+        else startLogin()
+    }
+
+    private fun startLogin() {
+        val intent = Intent(this, SignInActivity::class.java)
+        startActivity(intent)
+    }
+
+    private fun startMain() {
+        val intent = Intent(this, MainActivity::class.java)
+        startActivity(intent)
+    }
+
+    private fun setDelay() {
+        Handler(Looper.getMainLooper()).postDelayed({
+            val fadeAnim: Animation = AnimationUtils.loadAnimation(this, R.anim.splash_animation)
+            binding.imageView.startAnimation(fadeAnim)
+            checkAutoLogin()
+            finish()
+        }, 2000L)
+    }
+}
