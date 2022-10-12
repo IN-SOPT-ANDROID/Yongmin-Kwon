0d7c2ab7401b1a1c3f3031d806cb53c27e66c55d
권용민 <briandr@naver.com>
2022-10-12 23:07:59 +0900

    init bottom navigation control

--- app/src/main/java/org/sopt/sample/ui/main/HomeActivity.kt
+++ app/src/main/java/org/sopt/sample/ui/main/HomeActivity.kt
@@ -2,7 +2,11 @@ package org.sopt.sample.ui.main
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import org.sopt.sample.R
 import org.sopt.sample.databinding.ActivityHomeBinding
+import org.sopt.sample.ui.main.gallery.GalleryFragment
+import org.sopt.sample.ui.main.home.HomeFragment
+import org.sopt.sample.ui.main.search.SearchFragment
 
 class HomeActivity : AppCompatActivity() {
     private lateinit var binding: ActivityHomeBinding
@@ -11,5 +15,20 @@ class HomeActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityHomeBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        clickAnotherBottomNavi()
+    }
+
+    private fun clickAnotherBottomNavi() {
+        binding.bottomNaviHome.setOnItemSelectedListener {
+            val transaction = supportFragmentManager.beginTransaction()
+            when (it.itemId) {
+                R.id.menu_home -> transaction.replace(R.id.fragmentContainerHome, HomeFragment())
+                R.id.menu_search -> transaction.replace(R.id.fragmentContainerHome, SearchFragment())
+                R.id.menu_gallery -> transaction.replace(R.id.fragmentContainerHome, GalleryFragment())
+            }
+            transaction.commit()
+            true
+        }
     }
 }
