492b4313ead9409fb5dbc8cb4eafee2a4916f802
권용민 <briandr@naver.com>
2022-11-05 16:44:01 +0900

    retrofit service creator 생성

---
+++ app/src/main/java/org/sopt/sample/data/remote/ServiceFactory.kt
@@ -0,0 +1,17 @@
+package org.sopt.sample.data.remote
+
+import com.jakewharton.retrofit2.converter.kotlinx.serialization.asConverterFactory
+import kotlinx.serialization.json.Json
+import okhttp3.MediaType.Companion.toMediaType
+import retrofit2.Retrofit
+
+object ServiceFactory {
+    val retrofit: Retrofit by lazy {
+        Retrofit.Builder()
+            .baseUrl("http://3.39.169.52:3000/")
+            .addConverterFactory(Json.asConverterFactory("application/json".toMediaType()))
+            .build()
+    }
+
+    inline fun <reified T> create(): T = retrofit.create<T>(T::class.java)
+}
