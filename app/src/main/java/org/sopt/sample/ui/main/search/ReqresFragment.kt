13cc35ef237a54bf875f50c1535b89cc61bec01d
권용민 <briandr@naver.com>
2022-11-11 01:58:57 +0900

    feat(reqres): Reqres 서버통신 로직 구현

--- app/src/main/java/org/sopt/sample/ui/main/search/ReqresFragment.kt
+++ app/src/main/java/org/sopt/sample/ui/main/search/ReqresFragment.kt
@@ -5,7 +5,14 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import org.sopt.sample.R
+import org.sopt.sample.data.remote.ServicePool
+import org.sopt.sample.data.remote.entity.reqres.ResponseReqresDTO
 import org.sopt.sample.databinding.FragmentReqresBinding
+import org.sopt.sample.shortToast
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class ReqresFragment : Fragment() {
     private var _binding: FragmentReqresBinding? = null
@@ -17,5 +24,26 @@ class ReqresFragment : Fragment() {
     ): View? {
         _binding = FragmentReqresBinding.inflate(layoutInflater)
         return binding.root
+    }
+
+    private fun initReqresNetwork() {
+        val reqresService = ServicePool.reqresService
+        reqresService.getUsers(1).enqueue(
+            object : Callback<ResponseReqresDTO> {
+                override fun onResponse(
+                    call: Call<ResponseReqresDTO>,
+                    response: Response<ResponseReqresDTO>
+                ) {
+                    if (response.isSuccessful) {
+                        //TODO
+                    } else requireContext().shortToast(R.string.serverConnectResponseError)
+                }
+
+                override fun onFailure(call: Call<ResponseReqresDTO>, t: Throwable) {
+                    requireContext().shortToast(R.string.serverConnectOnFailure)
+                }
+
+            }
+        )
     }
 }
