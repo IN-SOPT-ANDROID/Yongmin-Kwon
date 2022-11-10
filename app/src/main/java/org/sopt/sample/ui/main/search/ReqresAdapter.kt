0e6f55c9609469b97f44b7d29dd125d2fcf520fc
권용민 <briandr@naver.com>
2022-11-11 02:36:40 +0900

    feat(reqres): ReqresViewHolder 로직 작성

--- app/src/main/java/org/sopt/sample/ui/main/search/ReqresAdapter.kt
+++ app/src/main/java/org/sopt/sample/ui/main/search/ReqresAdapter.kt
@@ -5,13 +5,16 @@ import android.view.ViewGroup
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
 import org.sopt.sample.data.remote.entity.reqres.ResponseReqresDTO
 import org.sopt.sample.databinding.ItemReqresBinding
 
 class ReqresAdapter : ListAdapter<ResponseReqresDTO.Data, ReqresAdapter.ReqresViewHolder>(ReqresComparator()) {
     class ReqresViewHolder(private val binding: ItemReqresBinding) : RecyclerView.ViewHolder(binding.root) {
         fun onBind(data: ResponseReqresDTO.Data) {
-            //TODO
+            Glide.with(binding.imageReqresProfile).load(data.avatar).into(binding.imageReqresProfile)
+            binding.textReqresName.text = data.firstName + " " + data.lastName
+            binding.textReqresEmail.text = data.email
         }
     }
 
