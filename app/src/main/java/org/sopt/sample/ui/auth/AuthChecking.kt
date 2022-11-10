49e32d2f46d7a9bf182413525f227a72e5d5f3e7
권용민 <briandr@naver.com>
2022-11-10 18:16:29 +0900

    feat(auth): 로그인 서버통신 로직 구현
    SignInActivity에서 로그인 서버통신하는 로직을 구현하고
    strings에 필요한 값들을 추가했다.
    이제 서버통신으로
    로그인을 구분하므로 AuthChecking에서 로그인과 관련된 로직들을 삭제했다.

--- app/src/main/java/org/sopt/sample/ui/auth/AuthChecking.kt
+++ app/src/main/java/org/sopt/sample/ui/auth/AuthChecking.kt
@@ -1,9 +1,5 @@
 package org.sopt.sample.ui.auth
 
-import android.content.Context
-import org.sopt.sample.R
-import org.sopt.sample.shortToast
-
 class AuthChecking {
     fun isSignUpEmailValid(email: String): Boolean {
         return if (email.isEmpty()) false
@@ -18,27 +14,6 @@ class AuthChecking {
         if (mbti.length < 4) return SHORT
         else if (mbti in mbtiList) return CORRECT
         else return STRANGE
-    }
-
-    fun isSignInValid(
-        context: Context, id: String?, pw: String?, inputId: String, inputPw: String
-    ): Boolean {
-        if (id == null) {
-            context.shortToast(R.string.needSignUp)
-            return false
-        } else if ((inputId != id) and (inputPw != pw)) {
-            context.shortToast(R.string.checkIdPw)
-            return false
-        } else if (inputId != id) {
-            context.shortToast(R.string.checkId)
-            return false
-        } else if (inputPw != pw) {
-            context.shortToast(R.string.checkPw)
-            return false
-        } else {
-            context.shortToast(R.string.succeedSignIn)
-            return true
-        }
     }
 
     private val mbtiList: List<String> = listOf(
