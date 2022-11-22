99b56783e51e01b64a32bb3edb0f759c51ea2ea5
brian <briandr@naver.com>
2022-11-23 04:08:34 +0900

    feat(ToastSnackBarExt): string파일의 id값이 아닌 그냥 String으로도 입력할 수 있도록 함수를 추가했다.

--- app/src/main/java/org/sopt/sample/util/ToastSnackBarExt.kt
+++ app/src/main/java/org/sopt/sample/util/ToastSnackBarExt.kt
@@ -5,17 +5,17 @@ import android.view.View
 import android.widget.Toast
 import com.google.android.material.snackbar.Snackbar
 
-fun View.defaultSnackbar(stringId: Int): Unit {
+fun View.defaultSnackbar(stringId: Int) {
     Snackbar.make(this, stringId, Snackbar.LENGTH_SHORT).show()
 }
 
-fun View.shortSnackbar(stringId: Int): Unit {
+fun View.shortSnackbar(stringId: Int) {
     Snackbar.make(this, stringId, Snackbar.LENGTH_SHORT).apply {
         anchorView = this@shortSnackbar
     }.show()
 }
 
-fun View.LongSnackbar(stringId: Int): Unit {
+fun View.LongSnackbar(stringId: Int) {
     Snackbar.make(this, stringId, Snackbar.LENGTH_LONG).apply {
         anchorView = this@LongSnackbar
     }.show()
@@ -23,6 +23,10 @@ fun View.LongSnackbar(stringId: Int): Unit {
 
 fun Context.shortToast(stringId: Int) {
     Toast.makeText(this, stringId, Toast.LENGTH_SHORT).show()
+}
+
+fun Context.shortToastString(text: String) {
+    Toast.makeText(this, text, Toast.LENGTH_SHORT).show()
 }
 
 fun Context.longToast(stringId: Int) {
