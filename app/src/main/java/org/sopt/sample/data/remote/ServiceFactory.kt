1d8683a706bd962eb13adad3c79e1e93234a6443
brian <briandr@naver.com>
2022-12-01 17:32:42 +0900

    로깅 인터셉터 추가

--- app/src/main/java/org/sopt/sample/data/remote/ServiceFactory.kt
+++ app/src/main/java/org/sopt/sample/data/remote/ServiceFactory.kt
@@ -3,16 +3,28 @@ package org.sopt.sample.data.remote
 import com.jakewharton.retrofit2.converter.kotlinx.serialization.asConverterFactory
 import kotlinx.serialization.json.Json
 import okhttp3.MediaType.Companion.toMediaType
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 
 object ServiceFactory {
     private const val AUTH_BASE_URL = "http://3.39.169.52:3000/"
     private const val REQRES_BASE_URL = "https://reqres.in/api/"
 
+    private val client by lazy {
+        OkHttpClient.Builder()
+            .addInterceptor(
+                HttpLoggingInterceptor().apply {
+                    level = HttpLoggingInterceptor.Level.BODY
+                }
+            ).build()
+    }
+
     val retrofitAuth: Retrofit by lazy {
         Retrofit.Builder()
             .baseUrl(AUTH_BASE_URL)
             .addConverterFactory(Json.asConverterFactory("application/json".toMediaType()))
+            .client(client)
             .build()
     }
 
@@ -20,13 +32,15 @@ object ServiceFactory {
         Retrofit.Builder()
             .baseUrl(REQRES_BASE_URL)
             .addConverterFactory(Json.asConverterFactory("application/json".toMediaType()))
+            .client(client)
             .build()
     }
 
-    //inline fun <reified T> create(): T = retrofitAuth.create<T>(T::class.java)
+    // inline fun <reified T> create(): T = retrofitAuth.create<T>(T::class.java)
 }
 
 object ServicePool {
     val authService: AuthService = ServiceFactory.retrofitAuth.create(AuthService::class.java)
-    val reqresService : ReqresService = ServiceFactory.retrofitReqres.create(ReqresService::class.java)
+    val reqresService: ReqresService =
+        ServiceFactory.retrofitReqres.create(ReqresService::class.java)
 }
